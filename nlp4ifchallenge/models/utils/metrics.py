7cacdf987f4d27031030ab9d4c395163c65fa82d
konstantinosKokos <konstantinos@riseup.net>
2021-03-22 16:48:10 +0100

    align metrics with official

--- nlp4ifchallenge/models/utils/metrics.py
+++ nlp4ifchallenge/models/utils/metrics.py
@@ -1,18 +1,27 @@
 from ...types import * 
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, hamming_loss
+from ..bert import preds_to_str
+
+
+def _round(x: float) -> float:
+    return round(x, 3)
 
 
 def get_metrics(preds: List[List[int]], labels: List[List[int]]) -> Dict[str, float]:
-    # todo: check their shitty code
+    preds_ = [preds_to_str(p).split('\t') for p in preds]
+    labels_ = [preds_to_str(l).split('\t') for l in labels]
+    per_column_preds = list(zip(*preds_))
+    per_column_labels = list(zip(*labels_))
+    per_column_metrics = [(f1_score(pcp, pcl, labels=['yes', 'no'], average='weighted'),
+                           recall_score(pcp, pcl, labels=['yes', 'no'], average='weighted'),
+                           precision_score(pcp, pcl, labels=['yes', 'no'], average='weighted'))
+                          for pcp, pcl in zip(per_column_preds, per_column_labels)]
+    f1s, ps, rs = list(zip(*per_column_metrics))
     return {'accuracy': round(accuracy_score(preds, labels), 3),
             'hamming': round(1 - hamming_loss(preds, labels), 3),
-            'f1': round(f1_score(preds, labels, average='weighted'), 3),
-            'r': round(recall_score(preds, labels, average='weighted'), 3),
-            'p': round(precision_score(preds, labels, average='weighted'), 3),
-            'per_column_f1': [round(pcf1, 3) for pcf1 in per_column_f1(preds, labels)]}
-
-
-def per_column_f1(preds: List[List[int]], labels: List[List[int]]) -> List[float]:
-    per_column_preds = list(zip(*preds))
-    per_column_labels = list(zip(*labels))
-    return [f1_score(pcp, pcl, average='weighted') for pcp, pcl in zip(per_column_preds, per_column_labels)]
+            'mean_f1': round(sum(f1s)/len(f1s), 3),
+            'mean_p': round(sum(ps)/len(ps), 3),
+            'mean_r': round(sum(rs)/len(rs), 3),
+            'column_wise':
+                [{'f1': _round(f1), 'p': _round(p), 'r': _round(r)} for f1, p, r in per_column_metrics]
+            }
