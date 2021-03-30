3219c99c406db53fd5e3e4f7f155489e25f192c1
gtziafas <pullmypill@hotmail.com>
2021-03-30 11:35:07 +0300

    mystery

--- nlp4ifchallenge/models/bert.py
+++ nlp4ifchallenge/models/bert.py
@@ -1,19 +1,31 @@
 from ..types import *
 from ..preprocessing import *
-from torch import tensor, long, stack, manual_seed
-from torch.nn.utils.rnn import pad_sequence as _pad_sequence
 from .utils.metrics import preds_to_str
+from .training import train_epoch, eval_epoch
+from torch import tensor, long, stack, manual_seed, save
+from torch.nn.utils.rnn import pad_sequence as _pad_sequence
 
 from transformers import AutoModel, AutoTokenizer
 from warnings import filterwarnings
 from adabelief_pytorch import AdaBelief
+import sys
+import os
+
+SAVE_PREFIX = '/data/s3913171'
+
+
+def sprint(s: str) -> None:
+    print(s)
+    sys.stdout.flush()
 
 
 class BERTLike(Module, Model):
-    def __init__(self, name: str, model_dim: int, dropout_rate: float = 0.5, max_length: Maybe[int] = None):
+    def __init__(self, name: str, model_dim: int = 768, dropout_rate: float = 0.5, 
+            max_length: Maybe[int] = None, token_name: Maybe[str] = None):
         super().__init__()
+        self.token_name = name if token_name is None else token_name
         self.core = AutoModel.from_pretrained(name)
-        self.tokenizer = AutoTokenizer.from_pretrained(name, use_fast=False)
+        self.tokenizer = AutoTokenizer.from_pretrained(self.token_name, use_fast=False)
         self.max_length = max_length
         self.dropout = Dropout(dropout_rate)
         self.classifier = Linear(model_dim, 7)
@@ -80,26 +92,40 @@ def make_model(name: str) -> BERTLike:
     # todo: find all applicable models
     if name == 'del-covid':
         return BERTLike(name='digitalepidemiologylab/covid-twitter-bert-v2', model_dim=1024)
-    if name == 'vinai-covid':
-        return BERTLike(name='vinai/bertweet-covid19-base-cased', model_dim=768, max_length=128)
-    if name == '':
-        pass
+    elif name == 'vinai-covid':
+        return BERTLike(name='vinai/bertweet-covid19-base-cased', max_length=128)
+    
+    # multi-lingual models
+    elif name == 'multi-bert':
+        return BERTLike(name='bert-base-multilingual-cased')
+    elif name == 'multi-xlm':
+        return BERTLike(name='xlm-roberta-base')
+    elif name == 'multi-xnli':
+        return BERTLike(name='joeddav/xlm-roberta-large-xnli', model_dim=1024)
+    elif name == 'multi-microsoft':
+        return BERTLike(name='microsoft/Multilingual-MiniLM-L12-H384', model_dim=384, token_name='xlm-roberta-base')
+    elif name == 'multi-sentiment':
+        return BERTLike(name='socialmediaie/TRAC2020_ALL_C_bert-base-multilingual-uncased')
+    elif name == 'multi-toxic':
+        return BERTLike(name='unitary/multilingual-toxic-xlm-roberta')
     else:
         raise ValueError(f'unknown name {name}')
 
 
 def train_bert(name: str,
-               train_path: str = './nlp4ifchallenge/data/english/covid19_disinfo_binary_english_train.tsv',
-               dev_path: str = './nlp4ifchallenge/data/english/covid19_disinfo_binary_english_dev_input.tsv',
-               test_path: str = '',
-               device: str = 'cuda',
-               batch_size: int = 16,
-               num_epochs: int = 10,
-               save_path = f'./nlp4ifchallenge/checkpoints',
-               with_class_weights: bool = True):
+               train_path: str,
+               dev_path: str,
+               test_path: str,
+               device: str,
+               batch_size: int,
+               num_epochs: int,
+               save_path: str,
+               with_class_weights: bool):
     save_path = '/'.join([save_path, name])
+    if not os.path.isdir(save_path):
+        os.mkdir(save_path)
 
-    torch.manual_seed(0)
+    manual_seed(0)
     filterwarnings('ignore')
 
     model = make_model(name).to(device)
@@ -110,22 +136,51 @@ def train_bert(name: str,
     dev_dl = DataLoader(model.tensorize_labeled(dev_ds), batch_size=batch_size,
                           collate_fn=lambda batch: collate_tuples(batch, model.tokenizer.pad_token_id), shuffle=False)
 
+    # if provided test path 
+    if test_path != '':
+        test_ds = read_labeled(test_path)
+        test_dl = DataLoader(model.tensorize_labeled(test_ds), batch_size=batch_size,
+                          collate_fn=lambda batch: collate_tuples(batch, model.tokenizer.pad_token_id), shuffle=False)
+
     class_weights = tensor([0.6223, 12.6667,  1.0594,  2.9561,  2.0473,  3.4653,  1.6374], device=device)
     #criterion = BCEWithLogitsLoss(pos_weight=class_weights)
     criterion = BCEWithLogitsLoss() if not with_class_weights else BCEWithLogitsLoss(pos_weight=class_weights)
     optimizer = AdaBelief(model.parameters(), lr=1e-05, weight_decay=1e-01, print_change_log=False)
 
-    train_log, dev_log = [], []
+    train_log, dev_log, test_log = [], [], []
     best = 0.
     for epoch in range(num_epochs):
         train_log.append(train_epoch(model, train_dl, optimizer, criterion, device))
-        print(train_log[-1])
+        sprint(train_log[-1])
         dev_log.append(eval_epoch(model, dev_dl, criterion, device))
-        print(dev_log[-1])
-        print('=' * 64)
+        sprint(dev_log[-1])
+        sprint('=' * 64)
         mean_f1 = dev_log[-1]['mean_f1']
         if mean_f1 > best:
             best = mean_f1
             faith = array([c['f1'] for c in dev_log[-1]['column_wise']])
-            torch.save(
+            save(
                 {'faith': faith, 'model_state_dict': model.state_dict()}, f'{save_path}/model.p')
+            # eval on test set for each new best model
+            if test_path != '':
+                test_log.append(eval_epoch(model, test_dl, criterion, device))
+                sprint('\nTEST\n')
+                sprint(test_log[-1])
+                sprint('=' * 64)
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--name', help='name of the BERT model to load', type=str)
+    parser.add_argument('-tr', '--train_path', help='path to the training data tsv', type=str, default='./nlp4ifchallenge/data/english/covid19_disinfo_binary_english_train.tsv')
+    parser.add_argument('-dev', '--dev_path', help='path to the development data tsv', type=str, default='./nlp4ifchallenge/data/english/covid19_disinfo_binary_english_dev_input.tsv')
+    parser.add_argument('-tst', '--test_path', help='path to the testing data tsv', type=str, default='')
+    parser.add_argument('-d', '--device', help='cpu or cuda', type=str, default='cuda')
+    parser.add_argument('-bs', '--batch_size', help='batch size to use for training', type=int, default=32)
+    parser.add_argument('-e', '--num_epochs', help='how many epochs of training', type=int, default=7)
+    parser.add_argument('-s', '--save_path', help='where to save best model', type=str, default=f'{SAVE_PREFIX}/nlp4ifchallenge/checkpoints')
+    parser.add_argument('--with_class_weights', action='store_true', help='use pre-computed weights for labels', default=False)
+
+    kwargs = vars(parser.parse_args())
+    train_bert(**kwargs)
