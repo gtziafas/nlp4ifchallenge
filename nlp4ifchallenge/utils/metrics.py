1fc373dba2dd9a28a9161ae7a97c7c076640693e
gtziafas <pullmypill@hotmail.com>
2021-04-05 21:09:57 +0300

    manually remove nans for accuracy+hamming

--- nlp4ifchallenge/utils/metrics.py
+++ nlp4ifchallenge/utils/metrics.py
@@ -1,4 +1,5 @@
 from ..types import * 
+from numpy import argwhere
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, hamming_loss
 
 
@@ -10,7 +11,7 @@ def preds_to_str(preds: List[int]) -> str:
     return '\t'.join(['nan' if preds[0] == 0 and 0 < i < 5 else 'yes' if p == 1 else 'no' for i, p in enumerate(preds)])
 
 
-def get_metrics(preds: List[List[int]], labels: List[List[int]]) -> Dict[str, Any]:
+def get_metrics(preds: List[List[int]], labels: List[List[int]], ignore_index: int = -1) -> Dict[str, Any]:
     preds_ = [preds_to_str(p).split('\t') for p in preds]
     labels_ = [preds_to_str(l).split('\t') for l in labels]
     per_column_preds = list(zip(*preds_))
@@ -20,8 +21,13 @@ def get_metrics(preds: List[List[int]], labels: List[List[int]]) -> Dict[str, An
                            precision_score(pcp, pcl, labels=['yes', 'no'], average='weighted'))
                           for pcp, pcl in zip(per_column_preds, per_column_labels)]
     f1s, ps, rs = list(zip(*per_column_metrics))
-    return {'accuracy': _round(accuracy_score(array(preds), array(labels)), labels=[1, 0]),
-            'hamming': _round(1 - hamming_loss(array(preds), array(labels)), labels=[1, 0]),
+
+    # manually remove ignored nans for accuracy and hamming computation 
+    preds, labels = array(preds), array(labels)
+    labels[labels == ignore_index] = 0
+    preds[argwhere(preds[:,0] == 0), 1:5] = 0
+    return {'accuracy': _round(accuracy_score(preds, labels)),
+            'hamming': _round(1 - hamming_loss(preds, labels)),
             'mean_f1': _round(sum(f1s)/len(f1s)),
             'mean_precision': _round(sum(ps)/len(ps)),
             'mean_recall': _round(sum(rs)/len(rs)),
