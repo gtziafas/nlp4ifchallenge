b60d9261430b5fdea153182e0777415c33aecffd
gtziafas <pullmypill@hotmail.com>
2021-04-05 15:01:59 +0300

    focal loss

--- nlp4ifchallenge/utils/loss.py
+++ nlp4ifchallenge/utils/loss.py
@@ -1,17 +1,17 @@
 from ..types import *
-from torch import Module, exp
-import torch.nn.functional as F
+from torch import Module, BCEWithLogitsLoss, exp
 
 
 class WeightedFocalLoss(Module):
     "Non weighted version of Focal Loss"
-    def __init__(self, alpha=.25, gamma=2, device: str):
+    def __init__(self, alpha=.25, gamma=2, device: str = 'cpu', class_weights: Maybe[Tensor] = None):
         super(WeightedFocalLoss, self).__init__()
         self.alpha = torch.tensor([alpha, 1-alpha]).to(device)
         self.gamma = gamma
+        self.bce = BCEWithLogitsLoss(reduction='none', pos_weights=class_weights)
 
     def forward(self, inputs: Tensor, targets: Tensor) -> Tensor:
-        BCE_loss = F.binary_cross_entropy_with_logits(inputs, targets, reduction='none')
+        BCE_loss = self.bce(inputs, targets) 
         targets = targets.long()
         at = self.alpha.gather(0, targets.view(-1))
         pt = exp(-BCE_loss)
