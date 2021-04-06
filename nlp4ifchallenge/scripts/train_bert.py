14fe41a89ac31b506473e7e7f8a8fb24541a1d53
gtziafas <pullmypill@hotmail.com>
2021-04-06 15:09:11 +0300

    default train path to new split

--- nlp4ifchallenge/scripts/train_bert.py
+++ nlp4ifchallenge/scripts/train_bert.py
@@ -73,7 +73,7 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('-n', '--name', help='name of the BERT model to load', type=str)
-    parser.add_argument('-tr', '--train_path', help='path to the training data tsv', type=str, default='./data/english/covid19_disinfo_binary_english_train.tsv')
+    parser.add_argument('-tr', '--train_path', help='path to the training data tsv', type=str, default='./data/english/covid19_disinfo_binary_english_train_bert.tsv')
     parser.add_argument('-dev', '--dev_path', help='path to the development data tsv', type=str, default='./data/english/covid19_disinfo_binary_english_dev_input.tsv')
     parser.add_argument('-tst', '--test_path', help='path to the testing data tsv', type=str, default='')
     parser.add_argument('-d', '--device', help='cpu or cuda', type=str, default='cuda')
